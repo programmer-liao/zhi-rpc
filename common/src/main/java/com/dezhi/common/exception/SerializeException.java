541ff74113ae2fb5272f0c34b088da7bd4f01594
programmer-liao <2380167274@qq.com>
2023-01-28 22:24:19 +0800

    feat:SerializeException

---
+++ common/src/main/java/com/dezhi/common/exception/SerializeException.java
@@ -0,0 +1,11 @@
+package com.dezhi.common.exception;
+
+/**
+ * @author liaodezhi
+ * @date 2023/1/28
+ */
+public class SerializeException extends RuntimeException {
+    public SerializeException(String message) {
+        super(message);
+    }
+}

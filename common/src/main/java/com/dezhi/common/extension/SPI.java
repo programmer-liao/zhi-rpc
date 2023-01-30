5b1e1d4667433195672aed4b543ddce9f699cee0
programmer-liao <2380167274@qq.com>
2023-01-30 22:01:38 +0800

    feat:SPI

---
+++ common/src/main/java/com/dezhi/common/extension/SPI.java
@@ -0,0 +1,15 @@
+package com.dezhi.common.extension;
+
+import java.lang.annotation.*;
+
+/**
+ * Service Provider Interface 服务提供接口注解
+ * @author liaodezhi
+ * @date 2023/1/30
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface SPI {
+
+}

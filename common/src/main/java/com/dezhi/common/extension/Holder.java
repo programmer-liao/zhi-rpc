a26f13b83f549dd22531199129edfdc5605efba8
programmer-liao <2380167274@qq.com>
2023-01-31 11:51:12 +0800

    feat:Holder

---
+++ common/src/main/java/com/dezhi/common/extension/Holder.java
@@ -0,0 +1,30 @@
+package com.dezhi.common.extension;
+
+/**
+ * 用于 缓存数据, 同时保证可见性
+ * @author liaodezhi
+ * @date 2023/1/31
+ */
+public class Holder<T> {
+
+    /**
+     * 值
+     */
+    private volatile T value;
+
+    /**
+     * 获取value
+     * @return value
+     */
+    private T get() {
+        return value;
+    }
+
+    /**
+     * 设置value
+     * @param value value
+     */
+    public void set(T value) {
+        this.value = value;
+    }
+}

083c60b6543b03637102f28e51f4f1a3a98a3cb0
programmer-liao <2380167274@qq.com>
2023-01-31 10:51:29 +0800

    feat:ZKServiceRegistryImpl

--- simple/src/main/java/com/dezhi/simple/registry/zk/ZkServiceRegistryImpl.java
+++ simple/src/main/java/com/dezhi/simple/registry/zk/ZkServiceRegistryImpl.java
@@ -1,19 +1,32 @@
 package com.dezhi.simple.registry.zk;
 
 import com.dezhi.simple.registry.ServiceRegistry;
+import com.dezhi.simple.registry.zk.util.CuratorUtils;
+import org.apache.curator.framework.CuratorFramework;
 
 import java.net.InetSocketAddress;
 
 /**
  * zookeeper实现服务注册服务
+ *
  * @author liaodezhi
  * @date 2023/1/30
  */
 public class ZkServiceRegistryImpl implements ServiceRegistry {
 
+    /**
+     * 注册服务
+     *
+     * @param rpcServiceName    rpc服务名
+     * @param inetSocketAddress 服务地址
+     */
     @Override
     public void registerService(String rpcServiceName, InetSocketAddress inetSocketAddress) {
-        String servicePath = Cu
+        // 获取服务路径
+        String servicePath = CuratorUtils.ZK_RESISTER_ROOT_PATH + "/" + rpcServiceName + inetSocketAddress.toString();
+        // 获取zkClient
+        CuratorFramework zkClient = CuratorUtils.getZkClient();
+        // 创建永久节点
+        CuratorUtils.createPersistentNode(zkClient, servicePath);
     }
-
 }

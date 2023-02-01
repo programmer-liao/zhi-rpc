8ffdbdf0a0a139979f525954fcf52b7fc0220247
programmer-liao <2380167274@qq.com>
2023-02-01 10:26:08 +0800

    feat:NettyRpcServer PORT

---
+++ simple/src/main/java/com/dezhi/simple/remoting/transport/netty/server/NettyRpcServer.java
@@ -0,0 +1,9 @@
+package com.dezhi.simple.remoting.transport.netty.server;
+
+/**
+ * @author liaodezhi
+ * @date 2023/2/1
+ */
+public class NettyRpcServer {
+    public static final int PORT = 9998;
+}

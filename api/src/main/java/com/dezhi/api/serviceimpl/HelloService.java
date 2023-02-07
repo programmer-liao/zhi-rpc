303b2f0239f50ec44bfe388a5fb87f5cf2c968bd
programmer-liao <2380167274@qq.com>
2023-02-07 11:12:40 +0800

    feat:HelloService

---
+++ api/src/main/java/com/dezhi/api/serviceimpl/HelloService.java
@@ -0,0 +1,16 @@
+package com.dezhi.api.serviceimpl;
+
+/**
+ * Hello接口
+ * @author liaodezhi
+ * @date 2023/2/7
+ */
+public interface HelloService {
+
+    /**
+     * hello方法
+     * @param hello Hello
+     * @return String
+     */
+    String hello(Hello hello);
+}

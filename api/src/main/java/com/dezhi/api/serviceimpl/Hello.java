3aae380392103b0acb1c07308e8ed1711f31df63
programmer-liao <2380167274@qq.com>
2023-02-07 11:11:52 +0800

    feat:Hello

---
+++ api/src/main/java/com/dezhi/api/serviceimpl/Hello.java
@@ -0,0 +1,30 @@
+package com.dezhi.api.serviceimpl;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * Hello类
+ * @author liaodezhi
+ * @date 2023/2/7
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Hello implements Serializable {
+
+    /**
+     * 消息
+     */
+    private String message;
+
+    /**
+     * 描述
+     */
+    private String description;
+}
